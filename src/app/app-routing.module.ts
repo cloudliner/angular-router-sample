de4eead80a67775983f782615b6e7ed03f86dc74
cloudliner <cloudliner.jp@gmail.com>
2019-04-21 17:09:32 +0900

    hierarchy component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BaseComponent } from './base/base.component';
+import { MainComponent } from './main/main.component';
+import { DetailComponent } from './detail/detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BaseComponent,
+    children: [
+      {
+        path: 'main/:id',
+        component: MainComponent,
+        children: [
+          {
+            path: 'detail/:sub-id',
+            component: DetailComponent,
+          }
+        ]
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BaseComponent } from './base/base.component';
+import { MainComponent } from './main/main.component';
+import { DetailComponent } from './detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BaseComponent,
+    MainComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
